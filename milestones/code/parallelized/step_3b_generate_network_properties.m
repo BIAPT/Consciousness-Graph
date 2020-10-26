82ac2ac20023d5997c69a63483c0b623869a69c6
raphaelchristin <70120121+raphaelchristin@users.noreply.github.com>
2020-10-26 16:30:22 -0400

    changed matlab files
    changed a command to run on slurm

--- milestones/code/parallelized/step_3b_generate_network_properties.m
+++ milestones/code/parallelized/step_3b_generate_network_properties.m
@@ -25,7 +25,7 @@ NUM_CORE = 9;%number of cores, maybe change later
 %Create a local cluster objects
 local_cluster = parcluster('local')
 %modify the JobStorageLocation to $SLURM_TMPDIR
-local_cluster.JobStorageLocation = strcat('/scratch/raphchr/', getenv('SLURM_JOB_ID'))
+pc.JobStorageLocation = strcat('/scratch/raphchr/', getenv('SLURM_JOB_ID'))
 %start the parallel pool
 parpool(local_cluster, NUM_CORE)
 
